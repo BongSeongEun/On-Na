ab1d09c2e8151dbbc86b29685bb913a6d00ddf80
봉성은 <bongfam2@naver.com>
2025-08-05 22:55:42 +0900

    컬럼 name 변경

--- backend/src/main/java/com/example/onna/model/ChatMessage.java
+++ backend/src/main/java/com/example/onna/model/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.example.onna.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -20,6 +21,7 @@ public class ChatMessage {
     private String roomId;
     private Long senderId;
     private String message;
+    @Column(name = "is_read")
     private boolean read;
     private LocalDateTime timestamp;
 }
